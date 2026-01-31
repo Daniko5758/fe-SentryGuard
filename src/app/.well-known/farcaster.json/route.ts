ac2826d77a2386d0bd508b1eb0ad3a00d5d2fe6b
Daniko Sutopo <sutopodaniko4@gmail.com>
2026-01-31 18:52:30 +0700

    Update account association credentials

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,12 +4,11 @@ export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL || "https://sentry-gate-app.vercel.app";
 
   const config = {
-    accountAssociation: {
-      // KOSONGKAN DULU, NANTI DIISI SETELAH DEPLOY PERTAMA
-      header: "",
-      payload: "",
-      signature: ""
-    },
+    "accountAssociation": {
+    "header": "eyJmaWQiOjI0Mjc5OTYsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg0ODNFMWVFYzY2NjNkMmVFODU1MTJiMTlFNkI0Q0IwRjNlRDA2MDNmIn0",
+    "payload": "eyJkb21haW4iOiJzZW50cnktZ2F0ZS1hcHAudmVyY2VsLmFwcCJ9",
+    "signature": "Yu2tA5RDRQbxU9jpHPrzIbkD9EqHgGE/9JD4JVjfQANCb4OtW2dLtHCkf9n45kJ0GgXoFftP557GWWwUyRFZ2Rs="
+  },
     miniapp: {
       version: "1",
       name: "SentryGate",
